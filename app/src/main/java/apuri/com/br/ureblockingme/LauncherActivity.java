d33905b6bc0fb42b499e487a778c903342a474ec
paulo.junior <apuridev@gmail.com>
2016-06-26 22:00:37 -0300

    added firebase authentication

---
+++ app/src/main/java/apuri/com/br/ureblockingme/LauncherActivity.java
@@ -0,0 +1,7 @@
+package apuri.com.br.ureblockingme;
+
+/**
+ * Created by paulo.junior on 26/06/2016.
+ */
+public class LauncherActivity {
+}
---
+++ app/src/main/java/apuri/com/br/ureblockingme/UreBlockingMeApplication.java
@@ -0,0 +1,7 @@
+package apuri.com.br.ureblockingme;
+
+/**
+ * Created by paulo.junior on 27/06/2016.
+ */
+public class UreBlockingMeApplication {
+}
